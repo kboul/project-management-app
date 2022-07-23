3964491f90fb365181444e626a8aef8bc613ced5
kboul <kos.voulgaridis@gmail.com>
2022-07-23 21:30:41 +0300

    Implement add, update, delete project mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,7 +4,8 @@ const {
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLList,
-  GraphQLNonNull
+  GraphQLNonNull,
+  GraphQLEnumType
 } = require("graphql");
 
 const ProjectModel = require("../models/Project");
@@ -98,6 +99,75 @@ const mutation = new GraphQLObjectType({
       },
       resolve(parent, args) {
         return ClientModel.findByIdAndRemove(args.id);
+      }
+    },
+    addProject: {
+      type: ProjectType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        description: { type: new GraphQLNonNull(GraphQLString) },
+        status: {
+          type: new GraphQLEnumType({
+            name: "ProjectStatus",
+            values: {
+              new: { value: "Not Started" },
+              progress: { value: "In Progress" },
+              completed: { value: "Completed" }
+            }
+          }),
+          defaultValue: "Not Started"
+        },
+        clientId: { type: new GraphQLNonNull(GraphQLID) }
+      },
+      resolve(parent, args) {
+        const project = new ProjectModel({
+          name: args.name,
+          description: args.description,
+          status: args.status,
+          clientId: args.clientId
+        });
+        return project.save();
+      }
+    },
+    deleteProject: {
+      type: ProjectType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLID) }
+      },
+      resolve(parent, args) {
+        return ProjectModel.findByIdAndRemove(args.id);
+      }
+    },
+    updateProject: {
+      type: ProjectType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: {
+          type: new GraphQLEnumType({
+            name: "ProjectStatusUpdate",
+            values: {
+              new: { value: "Not Started" },
+              progress: { value: "In Progress" },
+              completed: { value: "Completed" }
+            }
+          }),
+          defaultValue: "Not Started"
+        }
+      },
+      resolve(parent, args) {
+        return ProjectModel.findByIdAndUpdate(
+          args.id,
+          {
+            $set: {
+              name: args.name,
+              description: args.description,
+              status: args.description
+            }
+          },
+          { new: true }
+        );
       }
     }
   }
