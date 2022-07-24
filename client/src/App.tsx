03d70ea3f1b3184c5bb336aa3d8f8287b21ef685
kboul <kos.voulgaridis@gmail.com>
2022-07-24 13:50:20 +0300

    Configure apollo/client and include Navbar on App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,16 @@
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+import { Navbar } from "./components";
+
+const client = new ApolloClient({
+  uri: "http://localhost/graphql",
+  cache: new InMemoryCache()
+});
+
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <ApolloProvider client={client}>
+      <Navbar />
+    </ApolloProvider>
+  );
 }
