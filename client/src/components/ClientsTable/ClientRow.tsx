aa15be5761c68e78ece37a0a1e3f2056e662e2a6
kboul <kos.voulgaridis@gmail.com>
2022-08-12 16:21:24 +0300

    Add confirmation dialog before deleting a client

--- client/src/components/ClientsTable/ClientRow.tsx
+++ client/src/components/ClientsTable/ClientRow.tsx
@@ -1,17 +1,30 @@
 /* eslint-disable no-shadow */
+import { useState } from "react";
 import { useMutation } from "@apollo/client";
-import { IconButton, TableRow, TableCell } from "@mui/material";
+import {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  IconButton,
+  TableRow,
+  TableCell,
+  Button
+} from "@mui/material";
 import { FaTrash } from "react-icons/fa";
 
 import { DELETE_CLIENT } from "../../mutations/client";
 import { Client } from "../../models";
 import { GET_CLIENTS } from "../../queries";
+import AppDialog from "../AppDialog";
+import TransitionBottomCenter from "../TransitionBottomCenter";
 
 interface ClientRowProps {
   client: Client;
 }
 
 export default function ClientRow({ client }: ClientRowProps) {
+  const [dialogOpen, setDialogOpen] = useState(false);
+
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
     // refetchQueries: [{ query: GET_CLIENTS }], // refetches all data after deletion
@@ -30,18 +43,38 @@ export default function ClientRow({ client }: ClientRowProps) {
     }
   });
 
-  const handleClick = () => deleteClient();
+  const handleOk = () => deleteClient();
+  const handleDialogOpen = () => setDialogOpen(true);
+  const handleDialogClose = () => setDialogOpen(false);
 
   return (
-    <TableRow key={client.id}>
-      <TableCell>{client.name}</TableCell>
-      <TableCell>{client.email}</TableCell>
-      <TableCell>{client.phone}</TableCell>
-      <TableCell>
-        <IconButton onClick={handleClick}>
-          <FaTrash color="black" size="0.5em" />
-        </IconButton>
-      </TableCell>
-    </TableRow>
+    <>
+      <TableRow key={client.id}>
+        <TableCell>{client.name}</TableCell>
+        <TableCell>{client.email}</TableCell>
+        <TableCell>{client.phone}</TableCell>
+        <TableCell>
+          <IconButton onClick={handleDialogOpen}>
+            <FaTrash color="black" size="0.5em" />
+          </IconButton>
+        </TableCell>
+      </TableRow>
+      <AppDialog
+        keepMounted
+        onClose={handleDialogClose}
+        open={dialogOpen}
+        title=""
+        TransitionComponent={TransitionBottomCenter}>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you want to delete this client?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleOk}>Ok</Button>
+          <Button onClick={handleDialogClose}>Cancel</Button>
+        </DialogActions>
+      </AppDialog>
+    </>
   );
 }
