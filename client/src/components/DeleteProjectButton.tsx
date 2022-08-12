662fb3cfa13ca65835956fa4253931124ab256ca
kboul <kos.voulgaridis@gmail.com>
2022-08-12 12:13:37 +0300

    Create button and implement functionality for deleting a project

---
+++ client/src/components/DeleteProjectButton.tsx
@@ -0,0 +1,36 @@
+import { useMutation } from "@apollo/client";
+import { Box, Button } from "@mui/material";
+import { FaTrash } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
+import { DELETE_PROJECT } from "../mutations/project";
+import { GET_PROJECTS } from "../queries";
+
+interface DeleteProjectButtonProps {
+  id: number;
+}
+
+export default function DeleteProjectButton({ id }: DeleteProjectButtonProps) {
+  const navigate = useNavigate();
+
+  const [deleteProject] = useMutation(DELETE_PROJECT, {
+    variables: { id },
+    onCompleted: () => navigate("/"),
+    refetchQueries: [{ query: GET_PROJECTS }]
+  });
+
+  const handleClick = () => deleteProject();
+
+  return (
+    <Box display="flex" justifyContent="flex-end" mt={2}>
+      <Button
+        onClick={handleClick}
+        size="small"
+        startIcon={<FaTrash />}
+        style={{ cursor: "pointer" }}
+        variant="contained">
+        Delete project
+      </Button>
+    </Box>
+  );
+}
